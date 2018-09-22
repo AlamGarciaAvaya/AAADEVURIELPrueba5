5f2ce9a56a3f81a0569af3f141f39ddebaf1e8b1
Kinich <umansilla@avaya.com>
2018-09-21 19:38:42 -0500

    Primer Commit 21 de Septiembre 2018

---
+++ AAADEVURIELPrueba5/AAADEVURIELPrueba5-war/src/main/java/com/avaya/myproyect/AAADEVURIELPrueba5/CustomerBean.java
@@ -0,0 +1,10 @@
+package com.avaya.myproyect.AAADEVURIELPrueba5;
+
+public class CustomerBean {
+	 	public int accountnum = 0;
+	    public String  email ="";
+	    public String firstname="";
+	    public String lastname="";
+	    public String phone="";
+	    public String preference="";
+}
